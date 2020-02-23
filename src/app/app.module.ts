7ac83edda504b4f34e9ae844dc4aef74f665c971
David Wallace <david@ssssw0.com>
2020-02-23 08:43:27 -0600

    Rework from AngularFireDatabaseModule to AngularFirestoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AddressService } from './address/address.service';
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { environment } from '../environments/environment';
 import { AddressListComponent } from './address/address-list/address-list.component';
 import { AddressEditComponent } from './address/address-edit/address-edit.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
+    // AngularFireDatabaseModule
+  ],
   declarations: [
     AppComponent,
     AddressListComponent,
     AddressEditComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireDatabaseModule
-  ],
-  providers: [],
+  providers: [AddressService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
